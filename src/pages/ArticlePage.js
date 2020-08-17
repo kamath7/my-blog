66cf19683d013893ed4b7cdee2e0418eafbe8f13
Adithya Kamath <adithyakamath@Adithyas-MacBook-Air.local>
2020-08-17 20:32:37 +0530

    Article page now displays upvotes

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,22 +1,28 @@
-import React from "react";
-import articleContent from './article-content';
-import ArticlesList from '../components/ArticlesList';
+import React, { useState, useEffect } from "react";
+import articleContent from "./article-content";
+import ArticlesList from "../components/ArticlesList";
 import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
   const article = articleContent.find(article => article.name === name);
 
-  if(!article) return <NotFoundPage/>
+  const [articleInfo, setArticleInfo] = useState({upvotes:0, comments:[]});
+
+  useEffect(()=>{
+    setArticleInfo({upvotes:Math.ceil(Math.random() * 10)})
+  },[name]);
+  if (!article) return <NotFoundPage />;
   const otherArticles = articleContent.filter(article => article.name !== name);
   return (
     <div>
       <h1> {article.title}</h1>
-      {article.content.map((paragraph)=>(
-          <p key={paragraph}>{paragraph}</p>
+      <p>This post has been upvotes {articleInfo.upvotes} times</p>
+      {article.content.map(paragraph => (
+        <p key={paragraph}>{paragraph}</p>
       ))}
       <h3>Other Articles</h3>
-      <ArticlesList articles={otherArticles}/>
+      <ArticlesList articles={otherArticles} />
     </div>
   );
 };
