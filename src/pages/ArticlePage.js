ee99b470bff6ef0cc3a73ac0cfba453b2d6e7d01
Adithya Kamath <adithyakamath@Adithyas-MacBook-Air.local>
2020-08-20 02:34:33 +0530

    Blog phase 1 complete

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -2,25 +2,37 @@ import React, { useState, useEffect } from "react";
 import articleContent from "./article-content";
 import ArticlesList from "../components/ArticlesList";
 import NotFoundPage from "./NotFoundPage";
+import CommentsList from './CommentsList';
+import UpvotesSection from '../components/UpvotesSection';
+import AddCommentForm from "../components/AddCommentForm";
 
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
   const article = articleContent.find(article => article.name === name);
 
-  const [articleInfo, setArticleInfo] = useState({upvotes:0, comments:[]});
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
 
-  useEffect(()=>{
-    setArticleInfo({upvotes:Math.ceil(Math.random() * 10)})
-  },[name]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`/api/articles/${name}`);
+      const body = await result.json();
+      console.log(body)
+      setArticleInfo(body);
+    };
+    fetchData();
+  }, [name]);
   if (!article) return <NotFoundPage />;
   const otherArticles = articleContent.filter(article => article.name !== name);
   return (
     <div>
       <h1> {article.title}</h1>
-      <p>This post has been upvotes {articleInfo.upvotes} times</p>
+      <UpvotesSection articleName={name} upvotes={articleInfo.upvotes} setArticleInfo={setArticleInfo}/>
       {article.content.map(paragraph => (
         <p key={paragraph}>{paragraph}</p>
       ))}
+
+      <CommentsList comments={articleInfo.comments}/>
+      <AddCommentForm articleName={name} setArticleInfo={setArticleInfo}/>
       <h3>Other Articles</h3>
       <ArticlesList articles={otherArticles} />
     </div>
---
+++ src/pages/CommentsList.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const CommentsList = ({ comments }) => {
+  return (
+    <div>
+    <h3>Comments:</h3>
+      {comments.map((comment,key) => (
+        <div className="comment" key={key}>
+          
+          <h4>{comment.username}</h4>
+          <p>{comment.text}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CommentsList;
